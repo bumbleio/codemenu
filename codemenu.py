6deccb694c7931e0d53b7524fb10be9d41beba43
Daniel Weeks (Corporate) <U11164@palic.ads>
2018-10-26 15:23:52 -0500

    added list_projects fucnction and git cheatsheet

--- codemenu.py
+++ codemenu.py
@@ -3,16 +3,19 @@ import platform
 
 
 #log
-# Next - add project files and list code for a project
+# Next - clean up function list projects, once user selected projects it shoudl really list all files in teh project and allow the user to select one or all. 
+# Right now it assumes there is ony one script file and it has teh same name as teh parent directory with .py extension
 
 root_directory = os.getcwd()
 os_version = platform.system()
 
-# check os version 
+# check os version and set project directory
 if os_version == 'Windows':
     project_directory = root_directory + '\projects'
+    hackerrank_directory = root_directory + '\hackerrank'
 else:
     project_directory = root_directory + '/projects'
+    hackerrank_directory = root_directory + '/hackerrank'
 
 
 
@@ -22,12 +25,27 @@ def get_input():
 def list_settings():
     pass
 
-def list_projects():
-    project_list = os.listdir(path=project_directory)
+def list_projects(directory):
+    project_list = os.listdir(path=directory)
+    projectdict = {}
     count = 1
+    print("Project list ")
     for project in project_list:
         print(str(count) + ': ' + project)
+        projectdict[count] = project
         count += 1
+    # Need to create dictionary number : directory name
+    print(projectdict)
+    project_number = int(input('Please select project by number'))
+    project_directory_file = (projectdict.get(project_number))
+    
+    # this will wokr only for windows right now
+    file_read = directory + '\\' + project_directory_file + '\\' + project_directory_file + '.py'
+    open_file = open(file_read, 'r')
+    print(open_file.read())
+
+def show_code(dictionary):
+    pass
 
 
 
@@ -43,8 +61,12 @@ user_selection = (get_input())
 if user_selection == 1:
     list_settings()
 elif user_selection == 2:
-    list_projects()
+    list_projects(project_directory)
+elif user_selection == 3:
+    list_projects(hackerrank_directory)
     
+
+
 
 
 
