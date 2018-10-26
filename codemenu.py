25d3e9605f076b3ffb09830512a8b18adc03cb4e
Daniel Weeks (Corporate) <U11164@palic.ads>
2018-10-26 12:46:25 -0500

    added .gitkeep file to add epty directories

--- codemenu.py
+++ codemenu.py
@@ -1,9 +1,19 @@
 import os
+import platform
+
+
+#log
+# Next - add project files and list code for a project
 
 root_directory = os.getcwd()
-# This currently only works on windows. Slash will need to change for unix
-# need to put a if OS windows then or unix
-project_directory = root_directory + '\projects'
+os_version = platform.system()
+
+# check os version 
+if os_version == 'Windows':
+    project_directory = root_directory + '\projects'
+else:
+    project_directory = root_directory + '/projects'
+
 
 
 def get_input():
@@ -24,7 +34,11 @@ def list_projects():
 print('-------' * 3 )
 print('1: Settings')
 print('2: Show all Projects')
-print('3: ???????')
+print('3: Hacker Rank')
+print('4: Search projects by tag')
+print('5: Projects raspberrypi.org ') 
+print('6: Docker Files')
+print('Cheat Sheets')
 user_selection = (get_input())
 if user_selection == 1:
     list_settings()
