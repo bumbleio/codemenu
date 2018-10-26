dc963aa08a6acc86351bda461de9a5dded1491e3
Daniel Weeks (Corporate) <U11164@palic.ads>
2018-10-26 13:33:01 -0500

    added hacker rank capitalize.py

---
+++ hackerrank/capitalize/capitalize.py
@@ -0,0 +1,10 @@
+# This is a function in capitalize the first letter of a string
+# #capital
+# #capitalize
+# #title
+
+def solve(NameError):
+    return name.title()
+
+name = input("assign a lower case name")
+print(solve(name))
