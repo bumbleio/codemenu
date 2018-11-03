200570f63012e4cf11629b434cf23fbef1d1f92f
bumbleio <bumbleio@protonmail.com>
2018-11-03 19:53:18 -0400

    added function place_bets to twoup.py

--- projects/twoup/twoup.py
+++ projects/twoup/twoup.py
@@ -4,7 +4,10 @@ import time
 max_users = 4
 count_down_secs = 10
 roll = True
-user_set = set() # this is a set for no duplicate names
+user_set = {} # this is a set for no duplicate names
+bet = 5
+user_bets = {}
+
 
 
 def coin_flip():
@@ -17,11 +20,11 @@ def get_user_selection():
 def select_spinner():
     pass
 
-def add_user(new_user):
+def add_user(new_user, starting_balance=100):
     print("User count on active table = " + str(len(user_set)))
     # should use list or tuble so stop duplicate names
     if len(user_set) < max_users:
-        user_set.add(new_user)
+        user_set.update({new_user: starting_balance})
     else:
         print("Maximun users reached on this table")
     
@@ -29,16 +32,21 @@ def list_active_users():
     print(user_set)
 
 def remove_user(existing_user):
-    user_set.remove(existing_user)
+    user_set.pop(existing_user)
 
 def check_user_exists():
     # check if user exists or not
     pass
 
+def roll_validate(dice1 , dice2):
+    pass
 
+def place_you_bets():
+    # sets up peoples bet
+    for user in user_set:
 
-
-
+        user_bets.update({user : input(user + " place your bet: ")})
+    print(user_bets)
 
 
 while roll:
@@ -54,20 +62,17 @@ while roll:
         user_name = input("add user name to play: ")
         add_user(user_name)
     elif selection == '2':
-        user_name = input("reomve user from game: ")
+        user_name = input("remove user from game: ")
         remove_user(user_name)
     elif selection == '3':
         list_active_users()
     elif selection == 'r':
+        place_you_bets()
         coin1 = coin_flip()
         coin2 = coin_flip()
         print(coin1 + " " + coin2)
     elif selection == 'X':
         break
-
-
-
-
 
 ### needs to learn asyncio module to allow users to join in the 20 second interval
 
