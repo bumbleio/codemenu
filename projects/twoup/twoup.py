ff72553903d6344635dbc3fb61dda64d671664ea
bumbleio <bumbleio@protonmail.com>
2018-11-08 18:15:41 -0500

    added reconcile user balance, i have not included the house calulations and the logic needs addressing

--- projects/twoup/twoup.py
+++ projects/twoup/twoup.py
@@ -4,7 +4,8 @@ import time
 max_users = 4
 count_down_secs = 10
 roll = True
-user_set = {} 
+user_list = {}
+house_list = {}
 bet = 5
 user_bets = {}
 house_balance = int(1000)
@@ -23,18 +24,18 @@ def select_spinner():
     pass
 
 def add_user(new_user, starting_balance=100):
-    print("User count on active table = " + str(len(user_set)))
+    print("User count on active table = " + str(len(user_list)))
     # should use list or tuble so stop duplicate names
-    if len(user_set) < max_users:
-        user_set.update({new_user: starting_balance})
+    if len(user_list) < max_users:
+        user_list.update({new_user: starting_balance})
     else:
         print("Maximun users reached on this table")
     
 def list_active_users():
-    print(user_set)
+    print(user_list)
 
 def remove_user(existing_user):
-    user_set.pop(existing_user)
+    user_list.pop(existing_user)
     user_bets.pop(existing_user)
     
 def check_user_exists():
@@ -43,13 +44,11 @@ def check_user_exists():
 
 def place_you_bets():
     # sets up peoples bet
-    for user in user_set:
+    for user in user_list:
         heads_or_tails = input(user + " place your bet T for tails, H for heads: ")
         # vaidate heads or tails is selected
         user_bets.update({user : heads_or_tails})
         validate_bets(user, heads_or_tails)
-    
-
 
 def validate_bets(user , heads_or_tails):
     bet_value = user_bets[user]
@@ -59,30 +58,30 @@ def validate_bets(user , heads_or_tails):
         validate_bets(user , heads_or_tails)
         
 def reconcile_bets():
+    # need to reset the house list each time new bets are placed
+    reset_house_list()
     sum_of_bets_heads = 0
     sum_of_bets_tails = 0
-    print('final user bets ' + str(user_bets))
+    print('User bets: ' + str(user_bets))
     for user, bet in user_bets.items():
-        print("the bet is: " + bet)
         if bet == 'T':
             sum_of_bets_tails += 1
         elif bet == 'H':
             sum_of_bets_heads += 1
-    print('tailscount= ' + str(sum_of_bets_tails))
-    print('headscount= ' + str(sum_of_bets_heads))
     bet_balance = sum_of_bets_tails - sum_of_bets_heads
     if bet_balance != 0:
-
-    #abs function make a negative number positive and keeps a positive number positive
+        #abs function makes a negative number positive and keeps a positive number positive
         bet_balance_count = abs(int(bet_balance))
         for i in range(bet_balance_count):
             if int(bet_balance) > 0:
                 user_bets.update({'house' + str(i) : 'H'})
+                house_list.update({'house' + str(i) : 0})
             elif int(bet_balance) < 0:
                 user_bets.update({'house' + str(i) : 'T'})
+                house_list.update({'house' + str(i) : 0})
 
             # create house accounts with corresponding bets
-        print ("final bets are : " + str(user_bets))
+        print ("final bets with house to balance pool : " + str(user_bets))
     else:
         print('balance even')
 
@@ -93,20 +92,43 @@ def determine_winners_losers(coin1, coin2):
         for user, bet in user_bets.items():
             if bet == 'H':
                 print(user + " has won")
+                reconcile_user_balace(user , "won")
             else:
                 print(user + " has lost")
-        
+                reconcile_user_balace(user , "lost")
+
     elif coin1 == 'tail' and coin2 == 'tail':
         for user, bet in user_bets.items():
             if bet == 'T':
                 print(user + " has won")
+                reconcile_user_balace(user , "won")
+
             else:
                 print(user + " has lost")
+                reconcile_user_balace(user , "lost")
     else:
         print('no winner re-roll')
     
 
+def reconcile_user_balace(user, result):
+    if result == 'won':
+        new_user_balance = int(user_list[user]) + 5
+        print(new_user_balance)
+        user_list[user] = new_user_balance
+        
+    elif result == 'lost':
+        new_user_balance = int(user_list[user]) - 5
+        print(new_user_balance)
+        user_list[user] = new_user_balance
 
+def reset_house_list():
+    house_list = {}
+
+def list_active_bets():
+    print(user_bets)
+
+def list_house_user_balances():
+    print(house_list)
 
 
 
@@ -117,6 +139,7 @@ while roll:
     print("2: Remove User")
     print("3: List Active Users")
     print("4: List Active bets")
+    print("5: List House User bets")
     # have to merge list active bets and place your bets
     print("p: PLace your bets")
     print("r: Start Round")
@@ -132,6 +155,10 @@ while roll:
         remove_user(user_name)
     elif selection == '3':
         list_active_users()
+    elif selection == '4':
+        list_active_bets()
+    elif selection == '5':
+        list_house_user_balances()   
     elif selection == 'p':
         place_you_bets()
         reconcile_bets()
@@ -142,6 +169,6 @@ while roll:
     elif selection == 'X':
         break
 
-### needs to learn asyncio module to allow users to join in the 20 second interval
+# running the place your bets a 2nd time doesnt resetu the orginal house users if created in previous round because of uneven bet
 
 
